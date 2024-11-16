fd270783b12e3f806c27f60c90a306b2378372fe
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-16 00:12:33 -0300

    Updating dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,11 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.navigation.compose)
+    with(libs) {
+        implementation(io.insert.koin)
+        implementation(androidx.navigation.compose)
+    }
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
